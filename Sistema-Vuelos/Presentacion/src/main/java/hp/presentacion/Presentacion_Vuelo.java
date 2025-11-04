0fbf51bba2f880f0d25c4a278167773ac07adbbb
Carloss <luiscmg06@outlook.es>
2025-11-04 15:17:09 -0700

    Commit
    Paneles de usuario final
    falta panel de Seleccionar asientos
    -- metodos--

---
+++ Sistema-Vuelos/Presentacion/src/main/java/hp/presentacion/Presentacion_Vuelo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package hp.presentacion;
+
+/**
+ *
+ * @author HP
+ */
+public class Presentacion_Vuelo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
